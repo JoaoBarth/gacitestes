42121225cd735d530f5def223d19ceff5dbb4aca
João Barth <ossoxii@gmail.com>
2014-11-24 10:39:07 -0200

    Screen Content SSC + Timing

---
+++ source/App/TAppEncoder/encmain.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   encmain.h
+ * Author: joaobarth
+ *
+ * Created on 18 de Novembro de 2014, 16:39
+ */
+
+/// incluir variaveis globais para contagem
+
+extern float   clockTotalIBC;
+extern float   clockTotalPLT;
+extern float   clockTotalFastIBC;
+extern float   clockTotalHashME;
+extern float   clockTotalME;
+extern float   clockTotalIntra;
+
+
+////
+
+
